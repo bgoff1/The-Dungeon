353f6d8738c972c24f12f5363d7b927c4b6bfe7d
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-03 13:59:29 -0600

    Worked on next scene
    - Worked on next scene, initially allows you to pick a character, then will pull up combat.
    - Combat has text on the right and right now 4 buttons on the left.

---
+++ The Dungeon/Assets/Scripts/ChooseGender.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseGender : MonoBehaviour {
+
+	public GameObject nextPanel;
+
+	private Button maleButton;
+	private Button femaleButton;
+	void Start () {
+		registerButtons();
+	}
+	
+	private void registerButtons()
+	{
+		Button[] buttons = gameObject.GetComponentsInChildren<Button>();
+		foreach (Button b in buttons) {
+			if (b.name == "Male")
+			{
+				maleButton = b;
+			}
+			else if (b.name == "Female")
+			{
+				femaleButton = b;
+			}
+		}
+		maleButton.onClick.AddListener(setMaleCharacter);
+		femaleButton.onClick.AddListener(setFemaleCharacter);
+	}
+
+	private void setMaleCharacter()
+	{
+		gameObject.SetActive(false);
+		nextPanel.SetActive(true);
+	}
+
+	private void setFemaleCharacter()
+	{
+		gameObject.SetActive(false);
+		nextPanel.SetActive(true);
+	}
+}
